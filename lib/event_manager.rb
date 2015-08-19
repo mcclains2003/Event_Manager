760ec76a8af645d02e0e8028c6e5bb28d8280b98
mcclains2003 <mcclains2003@gmail.com>
2015-08-19 13:25:31 -0500

    Added () to the open feature for readability

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,7 +1,7 @@
 require "csv"
 puts "EventManager Initialized!"
 
-contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents = CSV.open("event_attendees.csv", headers: true, header_converters: :symbol)
 contents.each do |row|
   name = row[:first_name]
   puts name
